c417c5914a64adfef6c6c869fbe80dbf70bc3c09
Sanmonsua <sanmonsua@gmail.com>
2020-07-23 20:52:29 -0500

    Add : coin model

--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Coin(models.Model):
+    id = models.CharField(max_length=64, primary_key=True)
+    name = models.CharField(max_length=64)
+    symbol = models.CharField(max_length=64)
+    price = models.FloatField(default=0)
+    likes = models.IntegerField(default=0)
+    market_cap = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"{self.name} ({self.symbol})"
