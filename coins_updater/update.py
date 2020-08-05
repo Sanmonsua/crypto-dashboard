181b86b02c4ae26da039f2156c6389911bc4245f
Sanmonsua <sanmonsua@gmail.com>
2020-08-05 12:43:26 -0500

    Add : start updating coins when started

--- coins_updater/update.py
+++ coins_updater/update.py
@@ -3,6 +3,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from coins_updater import api
 
 def start():
+    api.update_coins()
     scheduler = BackgroundScheduler()
     scheduler.add_job(api.update_coins, 'interval', hours=3)
     scheduler.start()
