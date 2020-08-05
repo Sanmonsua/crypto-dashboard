b0731cb3e810e4764bb4190a8b59399337cd9e80
Sanmonsua <sanmonsua@gmail.com>
2020-08-05 13:11:44 -0500

    Edit : start the app interval scheduler first

--- coins_updater/update.py
+++ coins_updater/update.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from coins_updater import api
 
 def start():
-    api.update_coins()
     scheduler = BackgroundScheduler()
-    scheduler.add_job(api.update_coins, 'interval', hours=3)
+    scheduler.add_job(api.update_coins, 'interval', hours=2)
     scheduler.start()
+    api.update_coins()
